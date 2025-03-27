2f66cf3479c54bb5d26627a718a8d7b67d5d94fb
iharshit766 <harshi766766@gmail.com>
2025-03-27 14:41:07 +0530

    [Harshit] ADD: Defined product category constants

---
+++ com/bridgelabz/onlineMarketplace/Categories.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.onlineMarketplace;
+
+// Categories.java
+public class Categories {
+    public static final String FICTION = "Fiction";
+    public static final String NON_FICTION = "Non-Fiction";
+    public static final String MENS_WEAR = "Men's Wear";
+    public static final String LAPTOP = "Laptop";
+}
+
